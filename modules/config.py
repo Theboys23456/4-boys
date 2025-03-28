2e33316035e9011461baef4afea477d0194de21a
Theboys23456 <krishna93107437@gmail.com>
2025-03-28 19:51:58 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "owner"
-BOT_TOKEN = "7531978030:AAG2-YGULattMW4I2SulEk1YSc99mvqLRmo"
+BOT_TOKEN = "7333052013:AAGPIHa-bKWgvK0XlvkMbIzIjsSjt5fJ9jk"
 API_ID = 21567814
 API_HASH = "cd7dc5431d449fd795683c550d7bfb7e"
 MONGO_URI = "mongodb+srv://pathanupsc:SuXqSnNocq3HnhXZ@cluster0.bpopu.mongodb.net/?retryWrites=true&w=majority"
