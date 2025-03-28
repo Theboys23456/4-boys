823eacb3e7a75bb6c1d0e3f17da3ccc9f4907205
Theboys23456 <krishna93107437@gmail.com>
2025-03-28 19:50:11 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "owner"
+BOT_TOKEN = "7531978030:AAG2-YGULattMW4I2SulEk1YSc99mvqLRmo"
+API_ID = 21567814
+API_HASH = "cd7dc5431d449fd795683c550d7bfb7e"
+MONGO_URI = "mongodb+srv://pathanupsc:SuXqSnNocq3HnhXZ@cluster0.bpopu.mongodb.net/?retryWrites=true&w=majority"
